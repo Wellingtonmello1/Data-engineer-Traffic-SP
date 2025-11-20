f64657433b1133727c0cf1c2109ec77141484ef6
Wellington Melo <152233513+Wellingtonmello1@users.noreply.github.com>
2025-11-19 22:46:23 -0300

    Update transform_silver.py

--- transform_silver.py
+++ transform_silver.py
@@ -1,27 +1,46 @@
 import sys
+from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.sql.functions import *
+import boto3
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init(args["JOB_NAME"], args)
+job.init(args['JOB_NAME'], args)
 
-acc = spark.read.parquet("s3://traffic-sp/silver/accidents/")
-flow = spark.read.parquet("s3://traffic-sp/silver/traffic_flow/")
+# Caminhos
+bronze_path = "s3://traffic-sp/bronze/"
+silver_path = "s3://traffic-sp/silver/"
 
-gold = acc.join(flow, "location", "left") \
-          .groupBy("location") \
-          .agg(
-              countDistinct("accident_id").alias("accidents"),
-              avg("speed").alias("avg_speed")
-          )
+# Lista somente arquivos .json reais
+s3 = boto3.client("s3")
+bucket = "traffic-sp"
+prefix = "bronze/"
 
-gold.write.mode("overwrite").parquet("s3://traffic-sp/gold/traffic_summary/")
+response = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+
+json_files = [
+    obj["Key"].replace("bronze/", "")
+    for obj in response.get("Contents", [])
+    if obj["Key"].endswith(".json")
+]
+
+print("Arquivos detectados:", json_files)
+
+for file in json_files:
+    df = (
+        spark.read
+            .option("multiline", "true")
+            .json(bronze_path + file)
+    )
+
+    table_name = file.replace(".json", "")
+
+    df.write.mode("overwrite").parquet(f"{silver_path}{table_name}/")
 
 job.commit()
